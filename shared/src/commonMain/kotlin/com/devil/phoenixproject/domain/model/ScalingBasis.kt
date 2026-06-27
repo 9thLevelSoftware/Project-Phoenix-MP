722598523e5f4bad05a3f8e918e5caf1062a6de8
Devil <dasblueeyeddevil@gmail.com>
2026-06-26 20:09:39 -0400

    feat(1rm): add ScalingBasis enum + RoutineExercise.scalingBasis (#517)

---
+++ shared/src/commonMain/kotlin/com/devil/phoenixproject/domain/model/ScalingBasis.kt
@@ -0,0 +1,16 @@
+package com.devil.phoenixproject.domain.model
+
+/** Baseline a routine exercise's percentage weight scales from. */
+enum class ScalingBasis {
+    MAX_WEIGHT_PR,
+    MAX_VOLUME_PR,
+    ESTIMATED_1RM,
+    ;
+
+    companion object {
+        fun fromPrType(prType: PRType): ScalingBasis = when (prType) {
+            PRType.MAX_WEIGHT -> MAX_WEIGHT_PR
+            PRType.MAX_VOLUME -> MAX_VOLUME_PR
+        }
+    }
+}
