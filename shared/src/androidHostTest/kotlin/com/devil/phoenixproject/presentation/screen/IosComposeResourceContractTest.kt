543476cf41899aeb0650b8c1242daf6eb2c75174
Devil <dasblueyeddevil@gmail.com>
2026-09-17 09:58:35 -0400

    test(ios): parse Xcode resource script contract

--- shared/src/androidHostTest/kotlin/com/devil/phoenixproject/presentation/screen/IosComposeResourceContractTest.kt
+++ shared/src/androidHostTest/kotlin/com/devil/phoenixproject/presentation/screen/IosComposeResourceContractTest.kt
@@ -4,6 +4,7 @@ import com.devil.phoenixproject.testutil.readProjectFile
 import kotlin.test.Test
 import kotlin.test.assertContains
 import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
 
 class IosComposeResourceContractTest {
     @Test
@@ -13,11 +14,22 @@ class IosComposeResourceContractTest {
                 ?: readProjectFile("iosApp/PhoenixApp/PhoenixApp.xcodeproj/project.pbxproj"),
         )
 
-        assertContains(project, "KOTLIN_TARGET=\"iosSimulatorArm64\"")
-        assertContains(project, "KOTLIN_TARGET=\"iosArm64\"")
-        assertContains(project, "processedResources/${'$'}{KOTLIN_TARGET}/main/composeResources")
-        assertContains(project, "compose-resources/composeResources")
-        assertContains(project, "Processed Compose resources not found")
+        val shellScript = assertNotNull(
+            Regex(
+                """shellScript = \"(.*?)\";""",
+                setOf(RegexOption.DOT_MATCHES_ALL),
+            ).find(project),
+        ).groupValues[1]
+            .replace("\\n", "\n")
+            .replace("\\\"", "\"")
+
+        assertContains(shellScript, "KOTLIN_TARGET=\"iosSimulatorArm64\"")
+        assertContains(shellScript, "KOTLIN_TARGET=\"iosArm64\"")
+        assertContains(shellScript, "processedResources/${'$'}{KOTLIN_TARGET}/main/composeResources")
+        assertContains(shellScript, "compose-resources/composeResources")
+        assertContains(shellScript, "Processed Compose resources not found")
+        assertContains(shellScript, "if [ -d \"${'$'}SOURCE\" ]; then")
+        assertContains(shellScript, "cp -R \"${'$'}SOURCE/\" \"${'$'}DEST/\"")
         assertFalse(project.contains("$(SRCROOT)/compose-resources\\n"))
     }
 }
